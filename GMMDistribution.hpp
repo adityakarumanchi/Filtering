48eeebb2f2935e82c72bbf485467e4e54e7fea74
Aditya Karumanchi <karumanchi.1@osu.edu>
2023-12-26 16:33:01 +0530

    Split cpp files into header & source

--- GMMDistribution.hpp
+++ GMMDistribution.hpp
@@ -1,3 +1,6 @@
+#ifndef GMMDISTRIBUTION_H
+#define GMMDISTRIBUTION_H
+
 #include <iostream>
 #include <cmath>
 #include <numeric>
@@ -9,27 +12,13 @@ class GMMDistribution
 {
     public:
         double_t weight_sum_tol = 1e-4;
-        GMMDistribution(const std::vector<Eigen::RowVector<double_t, dim>>& m,
+        GMMDistribution(const std::vector<Eigen::Vector<double_t, dim>>& m,
             const std::vector<Eigen::Matrix<double_t, dim, dim>>& C,
-            const std::vector<double_t>& w) :
-        mu_vector{m}, Sigma_vector{C}, w_vector{w} {}
+            const std::vector<double_t>& w) : mu_vector{m}, Sigma_vector{C}, w_vector{w} {};
 
-        double_t probability(Eigen::RowVector<double, dim> X)
-        {
-            double p_of_x_ = 0;
-            if (size_check && weight_sum_check)
-            {
-                for (auto idx=0; idx<w_vector.size(); idx++)
-                {
-                    GaussianDistribution GMMComp(mu_vector[idx], Sigma_vector[idx]);
-                    p_of_x_ += w_vector[idx] * GMMComp.probability(X);
-                }
-            }
+        double_t probability(const Eigen::Vector<double_t, dim>& X);
 
-            return p_of_x_;
-        };
-
-        std::vector<Eigen::RowVector<double_t, dim>> mu_vector;
+        std::vector<Eigen::Vector<double_t, dim>> mu_vector;
         std::vector<Eigen::Matrix<double_t, dim, dim>> Sigma_vector;
         std::vector<double_t> w_vector;
         // Eigen::RowVector<double_t, dim> mu_vector[num_comp];
@@ -37,17 +26,12 @@ class GMMDistribution
         // double_t w_vector[num_comp];
 
     // private:
-        bool size_check = (mu_vector.size() == Sigma_vector.size() && mu_vector.size() == w_vector.size());
-        bool weight_sum_check = (std::abs(std::accumulate(w_vector.begin(), w_vector.end(), 0.0) - 1.0) <= weight_sum_tol);
+        // bool size_check = (mu_vector.size() == Sigma_vector.size() && mu_vector.size() == w_vector.size());
+        // bool weight_sum_check = (std::abs(std::accumulate(w_vector.begin(), w_vector.end(), 0.0) - 1.0) <= weight_sum_tol);
 
-        void size_check_error_msg()
-        {
-            if (!size_check)
-                std::cout << "Error: The number of means, covariance matrices and weights is not the same!";
-        }
-        void weight_sum_error_msg()
-        {
-            if (!weight_sum_check)
-                std::cout << "Error: The sum of the weights is not close to 1 (with tolerance of " << std::to_string(weight_sum_tol) << ").";
-        }
+        // void size_check_error_msg();
+        // void weight_sum_error_msg();
 };
+
+#include "GMMDistribution.cpp"
+#endif
--- GaussianDistribution.hpp
+++ GaussianDistribution.hpp
@@ -5,17 +5,12 @@
 class GaussianDistribution
 {
     public:
-        Eigen::VectorXd mu;
-        Eigen::MatrixXd Sigma;
-        GaussianDistribution(const Eigen::VectorXd& m, const Eigen::MatrixXd& C) : mu{m}, Sigma{C} {}
-
-        double_t probability(Eigen::VectorXd X) //https://en.wikipedia.org/wiki/Multivariate_normal_distribution#Density_function
-        {
-            double_t exponent = -0.5*(mu-X).transpose()*(Sigma.inverse())*(mu-X);
-            double_t p_of_x_ = pow(2*M_PI, -X.size()/2.0) * pow(Sigma.determinant(), -0.5) * std::exp(exponent);
-
-            return p_of_x_;
-        };
+        Eigen::VectorXd mu; /// Mean
+        Eigen::MatrixXd Sigma; /// Covariance
+        /// Constructor
+        GaussianDistribution(const Eigen::VectorXd& m, const Eigen::MatrixXd& C) : mu{m}, Sigma{C} {};
+        /// p(X)
+        double_t probability(const Eigen::VectorXd& X); //https://en.wikipedia.org/wiki/Multivariate_normal_distribution#Density_function
 
     // private:
     //     // To-do: check that Sigma is positive-definite and use it as condition for calculating probability.
